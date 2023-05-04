a6816b4dc0be5308186097059ecdc568cb020b77
Stepan Pazderka <stepan.pazderka@icloud.com>
2023-05-04 19:17:43 +0200

    REFACTOR: One Game Controller for all platforms

--- Breakout Shared/GameViewController.swift
+++ Breakout Shared/GameViewController.swift
@@ -5,16 +5,23 @@
 //  Created by Štěpán Pazderka on 26.04.2023.
 //
 
+#if os(OSX)
 import Cocoa
+typealias ViewController = NSViewController
+#endif
+#if os(iOS) || os(tvOS)
+import UIKit
+typealias ViewController = UIViewController
+#endif
 import SpriteKit
 import GameplayKit
 
-class GameViewController: NSViewController {
-    
+
+class GameViewController: ViewController {
     var boolMenuScreen = true
     var router: GameViewRouter!
     var cgSize: CGSize!
-
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -27,15 +34,18 @@ class GameViewController: NSViewController {
         skView.presentScene(menu)
         boolMenuScreen = true
         
+#if os(OSX)
         // Create a new tracking area for the SKView
         let trackingArea = NSTrackingArea(rect: skView.bounds, options: [.mouseEnteredAndExited, .activeAlways], owner: self, userInfo: nil)
         skView.addTrackingArea(trackingArea)
+#endif
         
         skView.ignoresSiblingOrder = true
         skView.showsFPS = true
         skView.showsNodeCount = true
     }
-
+    
+#if os(OSX)
     // Add a mouseEntered and mouseExited event handler to the SKView
     override func mouseEntered(with event: NSEvent) {
         if boolMenuScreen {
@@ -48,7 +58,8 @@ class GameViewController: NSViewController {
     override func mouseExited(with event: NSEvent) {
         NSCursor.unhide()
     }
-
+#endif
+    
     func presentGameView() {
         let scene = GameScene(size: cgSize, router: router)
         let skView = self.view as! SKView
